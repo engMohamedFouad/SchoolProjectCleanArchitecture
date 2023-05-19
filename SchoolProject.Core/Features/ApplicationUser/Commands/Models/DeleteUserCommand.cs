07f69a9838d4ddf4e83e7925b7bfc192ba7a369f
mohamed Fouad <mohamed.fouad.mohamed.hassan@gmail.com>
2023-05-19 15:42:51 +0300

    Added the Delete Endpoint

---
+++ SchoolProject.Core/Features/ApplicationUser/Commands/Models/DeleteUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SchoolProject.Core.Bases;
+
+namespace SchoolProject.Core.Features.ApplicationUser.Commands.Models
+{
+    public class DeleteUserCommand : IRequest<Response<string>>
+    {
+        public int Id { get; set; }
+        public DeleteUserCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
