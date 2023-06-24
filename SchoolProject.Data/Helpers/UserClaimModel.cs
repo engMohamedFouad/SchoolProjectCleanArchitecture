ac197aeffb54904e3e30f1ba24a3e7689ead6999
Mohamed Fouad <103780066+engMohamedFouad@users.noreply.github.com>
2023-06-24 13:21:11 +0200

    Authorization (#11)
    * Added The Authorization Branch
    Added the Create Roles EndPoint
    
    * Added Data Seeder Of Role And User
    
    * Added Authorize based On Role
    Modify the Claims in Project
    
    * Added Authorize based On Role
    
    ---------
    
    Co-authored-by: mohamed Fouad <mohamed.fouad.mohamed.hassan@gmail.com>

--- SchoolProject.Data/Helpers/UserClaimModel.cs
+++ SchoolProject.Data/Helpers/UserClaimModel.cs
@@ -6,5 +6,6 @@
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Role { get; set; }
     }
 }
