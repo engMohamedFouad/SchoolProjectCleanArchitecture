1b138064ef4d4f7548017e975d0ac83faa3365d6
Mohamed Fouad <103780066+engMohamedFouad@users.noreply.github.com>
2023-07-28 16:12:51 +0200

    Filters And CurrentUser (#20)
    * Added the SendEmail Endpoint
    Added the EmailsService
    
    * Added Reset Password Endpoint
    Added Send Reset Password Code
    Added ConfirmEmail Endpoint
    
    * Edit the Route in ConfirmResetPassword To ConfirmResetPasswordCode
    
    * Added the Filters And CurrentUserService
    
    ---------
    
    Co-authored-by: mohamed Fouad <mohamed.fouad.mohamed.hassan@gmail.com>

---
+++ SchoolProject.Core/Filters/AuthFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SchoolProject.Data.Entities.Identity;
+using SchoolProject.Service.AuthServices.Interfaces;
+
+namespace SchoolProject.Core.Filters
+{
+    public class AuthFilter : IAsyncActionFilter
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly UserManager<User> _userManager;
+        public AuthFilter(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated==true)
+            {
+                var roles = await _currentUserService.GetCurrentUserRolesAsync();
+                if (roles.All(x => x!="User"))
+                {
+                    context.Result=new ObjectResult("Forbidden")
+                    {
+                        StatusCode = StatusCodes.Status403Forbidden
+                    };
+                }
+                else
+                {
+                    await next();
+                }
+
+            }
+        }
+    }
+}
+
---
+++ SchoolProject.Service/AuthServices/Implementations/CurrentUserService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using SchoolProject.Data.Entities.Identity;
+using SchoolProject.Data.Helpers;
+using SchoolProject.Service.AuthServices.Interfaces;
+
+namespace SchoolProject.Service.AuthServices.Implementations
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+
+        #region Fields
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<User> _userManager;
+        #endregion
+        #region Constructors
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor, UserManager<User> userManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+        }
+        #endregion
+        #region Functions
+        public int GetUserId()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.Claims.SingleOrDefault(claim => claim.Type==nameof(UserClaimModel.Id)).Value;
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+            return int.Parse(userId);
+        }
+
+        public async Task<User> GetUserAsync()
+        {
+            var userId = GetUserId();
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            if (user == null)
+            { throw new UnauthorizedAccessException(); }
+            return user;
+        }
+
+        public async Task<List<string>> GetCurrentUserRolesAsync()
+        {
+            var user = await GetUserAsync();
+            var roles = await _userManager.GetRolesAsync(user);
+            return roles.ToList();
+        }
+        #endregion
+    }
+}
