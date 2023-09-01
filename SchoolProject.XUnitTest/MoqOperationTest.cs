6218ac57de28da138705adb92b1a91326ccea5cf
mohamed Fouad <mohamed.fouad.mohamed.hassan@gmail.com>
2023-09-01 09:43:13 +0300

    Added the Moq and xunit To Project

---
+++ SchoolProject.XUnitTest/MoqOperationTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Moq;
+using SchoolProject.XUnitTest.Models;
+using SchoolProject.XUnitTest.MoqTest;
+
+namespace SchoolProject.XUnitTest
+{
+    public class MoqOperationTest
+    {
+        private readonly Mock<List<Car>> _mockService = new();
+        [Fact]
+        public void Add_Car()
+        {
+            //Arrange
+            var car = new Car() { Id=2, Name="Toyota", Color="Red" };
+            var carmoqService = new CarMoqService(_mockService.Object);
+            //act
+            var addResult = carmoqService.AddCar(car);
+            var carList = carmoqService.GetAll();
+            //Assert
+            addResult.Should().BeTrue();
+            carList.Should().NotBeNull();
+            carList.Should().HaveCount(1);
+        }
+        //[Fact]
+        //public void Remove_Car()
+        //{
+        //    //Arrange
+        //    var car = new Car() { Id=2, Name="Toyota", Color="Red" };
+        //    //act
+        //    var removeResult = _carMoqService.RemoveCar(2);
+        //    var carList = _carMoqService.GetAll();
+        //    //Assert
+        //    carList.Should().HaveCount(1);
+        //}
+
+    }
+}
---
+++ SchoolProject.XUnitTest/MoqTest/CarMoqService.cs
@@ -0,0 +1,32 @@
+﻿using SchoolProject.XUnitTest.Models;
+
+namespace SchoolProject.XUnitTest.MoqTest
+{
+    public class CarMoqService : ICarMoqService
+    {
+        public List<Car> carList;
+
+        public CarMoqService(List<Car> cars)
+        {
+            carList = cars;
+        }
+        public bool AddCar(Car car)
+        {
+            carList.Add(car);
+            return true;
+        }
+
+        public List<Car> GetAll()
+        {
+            return carList;
+        }
+
+        public bool RemoveCar(int? id)
+        {
+            if (id == null) return false;
+            var car = carList.Find(x => x.Id == id);
+            if (car == null) return false;
+            return carList.Remove(car);
+        }
+    }
+}
