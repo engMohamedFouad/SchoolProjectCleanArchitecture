1f4515dbc3cf31ad5626b598ae2ffc551916e124
mohamed Fouad <mohamed.fouad.mohamed.hassan@gmail.com>
2023-09-07 17:37:10 +0300

    Added the GetStudentById Test Method

--- SchoolProject.XUnitTest/CoreTests/Students/Queries/StudentQueryHandlerTest.cs
+++ SchoolProject.XUnitTest/CoreTests/Students/Queries/StudentQueryHandlerTest.cs
@@ -9,6 +9,7 @@ using SchoolProject.Core.Mapping.Students;
 using SchoolProject.Core.Resources;
 using SchoolProject.Data.Entities;
 using SchoolProject.Service.Abstracts;
+using System.Net;
 
 namespace SchoolProject.XUnitTest.CoreTests.Students.Queries
 {
@@ -30,7 +31,7 @@ namespace SchoolProject.XUnitTest.CoreTests.Students.Queries
             _mapperMock=new Mapper(configuration);
         }
         [Fact]
-        public async Task handle_StudentList_Should_NotNull_And_NotEmpty()
+        public async Task Handle_StudentList_Should_NotNull_And_NotEmpty()
         {
             //Arrange
             var studentList = new List<Student>()
@@ -48,6 +49,52 @@ namespace SchoolProject.XUnitTest.CoreTests.Students.Queries
             result.Data.Should().NotBeNullOrEmpty();
             result.Succeeded.Should().BeTrue();
             result.Data.Should().BeOfType<List<GetStudentListResponse>>();
+        }
+        [Theory]
+        [InlineData(3)]
+        //[InlineData(2)]
+        public async Task Handle_StudentById_when_Student_NotFound_Return_StatusCode404(int id)
+        {
+            //Arrange
+            var department = new Department() { DID=1, DNameAr="هندسة البرمجيات", DNameEn="SE" };
+            var studentList = new List<Student>()
+            {
+                new Student(){ StudID=1, Address="Alex", DID=1, NameAr="محمد",NameEn="mohamed", Department=department},
+                new Student(){ StudID=2, Address="Cairo", DID=1, NameAr="علي",NameEn="Ali", Department=department}
+            };
+            var query = new GetStudentByIDQuery(id);
+            _studentServiceMock.Setup(x => x.GetStudentByIDWithIncludeAsync(id)).Returns(Task.FromResult(studentList.FirstOrDefault(x => x.StudID==id)));
+
+            var handler = new StudentQueryHandler(_studentServiceMock.Object, _mapperMock, _localizerMock.Object);
+
+            //Act
+            var result = await handler.Handle(query, default);
+            //Assert
+            result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+        [Theory]
+        [InlineData(1)]
+        // [InlineData(2)]
+        public async Task Handle_StudentById_when_Student_Found_Return_StatusCode200(int id)
+        {
+            //Arrange
+            var department = new Department() { DID=1, DNameAr="هندسة البرمجيات", DNameEn="SE" };
+            var studentList = new List<Student>()
+            {
+                new Student(){ StudID=1, Address="Alex", DID=1, NameAr="محمد",NameEn="mohamed", Department=department},
+                new Student(){ StudID=2, Address="Cairo", DID=1, NameAr="علي",NameEn="Ali", Department=department}
+            };
+            var query = new GetStudentByIDQuery(id);
+            _studentServiceMock.Setup(x => x.GetStudentByIDWithIncludeAsync(id)).Returns(Task.FromResult(studentList.FirstOrDefault(x => x.StudID==id)));
+
+            var handler = new StudentQueryHandler(_studentServiceMock.Object, _mapperMock, _localizerMock.Object);
+
+            //Act
+            var result = await handler.Handle(query, default);
+            //Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Data.StudID.Should().Be(id);
+            result.Data.Name.Should().Be("mohamed");
         }
     }
 }
